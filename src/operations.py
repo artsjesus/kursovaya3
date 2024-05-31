ce44575aa3e76ed587c1d728cf04f176728fa486
Konstantin Maurin <artsjesus7@gmail.com>
2024-05-31 19:33:37 +0300

    add __repr__ and __str__

--- src/operations.py
+++ src/operations.py
@@ -1,14 +1,17 @@
 import re
 
 
-class Transaction():
-    def __init__(self, dat, description, to, amount, name):
-        self.date = dat
+class Transaction:
+    def __init__(self, date, description, to, amount, name):
+        self.date = date
         self.description = description
         self.to = to
         self.amount = amount
         self.name = name
         self.from_ = None
+
+    def __repr__(self):
+        return f"{self.date} {self.description} {self.to} {self.amount} {self.name} {self.from_}"
 
     def name_to(self):
         """Куда переводим"""
@@ -18,7 +21,6 @@ class Transaction():
         """Номер счета на который переводим"""
         return re.sub("[\D]", "", self.to)
 
-
     def card_name_from(self):
         """Название карты"""
         return re.sub("[^a-zA-ZА-Яа-я\s]", "", self.from_)
@@ -27,3 +29,5 @@ class Transaction():
         """Номер карты счета с которого переводим"""
         return re.sub("[\D]", "", self.from_)
 
+    def __str__(self):
+        return f"{self.date} {self.description} {self.to} {self.amount} {self.name} {self.from_}"
